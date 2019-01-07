0c06a2490faa9c96fc37a06f80b97b017ecd1875
admin <admin@admindeMacBook-Pro-4.local>
2019-01-07 19:39:50 +0800

    # 请提交包含正确的信息

--- LXGitTest/LXGitTest/AppDelegate.m
+++ LXGitTest/LXGitTest/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"0000");
+    NSLog(@"0001");
     return YES;
 }
 
